c7d2b881061383e8bb8846c33f35cfb220aecd2d
Quentin <Quentin@10.133.128.99>
2016-05-24 15:33:12 +0200

    phase 3 bis bis bis

--- src/jpu2016/dogfight/model/IArea.java
+++ src/jpu2016/dogfight/model/IArea.java
@@ -1,10 +1,12 @@
 package jpu2016.dogfight.model;
 
-import java.awt.Image;
-
 public interface IArea {
-	public Dimension getDimension();
+	public Dimension getDimension(){
+		
+	}
 	
-	public Image getImage();
-	
+	public Image getImage(){
+		
+	}
+
 }
