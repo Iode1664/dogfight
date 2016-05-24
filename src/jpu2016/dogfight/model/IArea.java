466316241df2f965619539326ee0785090c09f11
clément <clment@PC001>
2016-05-24 10:02:32 +0200

    Phase2 clément Moreau

---
+++ src/jpu2016/dogfight/model/IArea.java
@@ -0,0 +1,12 @@
+package jpu2016.dogfight.model;
+
+public interface IArea {
+	public getDimension(){
+		
+	}
+	
+	public getImage(){
+		
+	}
+
+}
---
+++ src/jpu2016/dogfight/model/IMobile.java
@@ -0,0 +1,34 @@
+package jpu2016.dogfight.model;
+
+public interface IMobile {
+	
+	public getDirection(){
+		
+	}
+
+	public void setDirection (Direction direction){
+		
+	}
+	
+	public getDimension(){
+		
+	}
+
+	public getWidth(){
+		
+	}
+	
+	public getHeight(){
+		
+	}
+	
+	public getSpeed(){
+		
+	}
+	
+	public getImage(){
+		
+	}
+	
+	
+}
