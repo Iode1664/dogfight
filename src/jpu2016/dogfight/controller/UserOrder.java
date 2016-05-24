1cf42352c3f4b0fd4b66f8ffef0992aa23d5cd4f
Quentin <Quentin@10.133.128.99>
2016-05-24 11:20:11 +0200

    Phase 2

---
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,22 @@
+package jpu2016.dogfight.controller;
+
+public abstract class UserOrder implements IUserOrder {
+	private int player;
+	
+	public UserOrder (int player, Order order){
+		
+	}
+	
+	public Order getOrder() {
+		
+	}
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(int player) {
+		this.player = player;
+	}
+
+}
