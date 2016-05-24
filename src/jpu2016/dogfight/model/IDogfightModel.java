68fd9901f1b5fa42452d943d4b6a14c628bd2de2
clément <clment@PC001>
2016-05-24 14:58:46 +0200

    Phase

--- src/jpu2016/dogfight/model/IDogfightModel.java
+++ src/jpu2016/dogfight/model/IDogfightModel.java
@@ -1,33 +1,21 @@
 package jpu2016.dogfight.model;
 
+import java.util.ArrayList;
+
 public interface IDogfightModel {
 	
-	public IArea getIArea(){
-	
-	}
+	public IArea getIArea();
 
-	public void buildArea(Dimension dimension){
+	public void buildArea(Dimension dimension);
 	
-	}
+	public void addMobile(IMobile Mibile);
 	
-	public void addMobile(IMobile Mibile){
-		
-	}
+	public void removeMobile(IMobile Mobile);
 	
-	public void removeMobile(IMobile Mobile){
-		
-	}
+	public ArrayList<IMobile> getMobiles();
 	
-	public ArrayList<IMobile> getMobiles(){
-		
-	}
+	public IMobile getMobileByPlayer(int player);
 	
-	public IMobile getMobileByPlayer(int player){
-		
-	}
-	
-	public void setMobilesHavesMoved(){
-		
-	}
+	public void setMobilesHavesMoved();
 }
 
